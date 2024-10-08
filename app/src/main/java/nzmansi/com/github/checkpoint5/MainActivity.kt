feec68961b1fe51ddf265f2799d025ebbc9e3fac
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-07 22:09:12 -0300

    Limpando os códigos não necessários na MainActivity.kt

--- app/src/main/java/nzmansi/com/github/checkpoint5/MainActivity.kt
+++ app/src/main/java/nzmansi/com/github/checkpoint5/MainActivity.kt
@@ -15,29 +15,5 @@ import nzmansi.com.github.checkpoint5.ui.theme.Checkpoint5Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Checkpoint5Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Checkpoint5Theme {
-        Greeting("Android")
     }
 }
