0c8cfcbb97ad2e040cdb9c9b1fe3ded8e765dfe2
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-07 22:32:17 -0300

    Criando o payload de resposta da API aberta do Bitcoin

---
+++ app/src/main/java/nzmansi/com/github/checkpoint5/model/TickerResponse.kt
@@ -0,0 +1,15 @@
+package nzmansi.com.github.checkpoint5.model
+
+class TickerResponse(
+    val ticker: Ticker
+)
+
+class Ticker(
+    val high: String,
+    val low: String,
+    val vol: String,
+    val last: String,
+    val buy: String,
+    val sell: String,
+    val date: Long
+)
